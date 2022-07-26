5258a76fbc56e6c3124f09c0702f374081f61207
Crispthofer A. Rincon M <alex877@gmail.com>
2022-07-26 14:42:10 -0500

    [Add] MapsApp folder structure + Files not necessary were deleted + Context files were created

---
+++ src/MapsApp.tsx
@@ -0,0 +1,12 @@
+import { PlacesProvider } from "./context"
+
+
+const MapsApp = () => {
+  return (
+    <PlacesProvider>
+        <h1>Hello World... Again</h1>
+    </PlacesProvider>
+  )
+}
+
+export default MapsApp
---
+++ src/context/index.ts
@@ -0,0 +1,5 @@
+// Allow to export all Context without using in other files
+
+export { PlacesContext } from "./places/PlacesContext";
+export { PlacesProvider } from "./places/PlacesProvider";
+
